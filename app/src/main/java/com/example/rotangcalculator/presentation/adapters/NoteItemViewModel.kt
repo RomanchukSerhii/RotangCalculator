dcffda23f8d8e20c2a3cc3d05a8a853c593a0033
unknown <serhiiromanchuk@gmail.com>
2023-06-25 15:23:37 +0300

    Add and implement NoteItemViewModel

---
+++ app/src/main/java/com/example/rotangcalculator/presentation/adapters/NoteItemViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.rotangcalculator.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rotangcalculator.databinding.NoteItemBinding
+import com.example.rotangcalculator.domain.models.NoteItem
+
+class NoteItemViewModel(
+    private val binding: NoteItemBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(noteItem: NoteItem) {
+        with(binding) {
+            tvNoteTitle.text = noteItem.title
+            tvHighDiameter.text = noteItem.highDiameter.toString()
+            tvLowerDiameter.text = noteItem.lowerDiameter.toString()
+            tvLength.text = noteItem.length.toString()
+            tvWidth.text = noteItem.width.toString()
+            tvNumber.text = noteItem.number.toString()
+            tvResultValue.text = noteItem.result
+        }
+    }
+}
